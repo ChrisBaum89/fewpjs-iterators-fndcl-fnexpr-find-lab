6104062d757dcee55c2cea92a59d60e45b9c5d11
Chris Baum <Christopher.Baum@valpo.edu>
2022-03-08 21:10:47 -0500

    commit tree 573b97c14f2e4949108a729892b06f414a37d52c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+function superbowlWin(record) {
+  let result = record.find(record => record.result === "W")
+  //return year if result is truthy, else return undefined
+  return !!result ? result.year : undefined
+}
+
+superbowlWin
